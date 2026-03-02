f289df1d72814cd3b81e19f63f713245da1c066c
Eduardo Valero Molina <121091822+Edu646@users.noreply.github.com>
2026-03-03 00:02:32 +0100

    Update Layout_Admin.jsx

--- frontend/gomusic/src/Layout_Admin.jsx
+++ frontend/gomusic/src/Layout_Admin.jsx
@@ -1,8 +1,16 @@
+// Layout_Admin.jsx
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Nav_Admin from "./Nav_Admin";
 import "./Layout_Admin.css";
 
 export default function Layout_Admin() {
-  
+  return (
+    <div className="layout-admin">
+      <Nav_Admin />
+      <main className="admin-content">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
